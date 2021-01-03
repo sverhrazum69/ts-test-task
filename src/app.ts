f2bcf8e3b48a5bd1577fc57fcac11f081e075cf3
Nikita <n.osypa666@gmail.com>
2021-01-03 18:29:31 +0200

    added api routing

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,46 @@
+import express from 'express';
+import bodyParser from 'body-parser';
 import TeacherCrudOperations from './db';
 
+const app = express();
 const teacherCrudOperations = new TeacherCrudOperations();
 
-teacherCrudOperations.delete(3).then((data) => {
-    console.log(data);
-}).catch((err) => {
-    console.error(err);
+app.use(bodyParser.json());
+
+app.post('/create', async (req, res) => {
+    try {
+        const createdTeacher = await teacherCrudOperations.create(req.body);
+        return res.status(201).json(createdTeacher);
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
 });
+
+app.get('/retrieve', async (req, res) => {
+    try {
+        const retrievedTeachers = await teacherCrudOperations.retrieve(req.query);
+        return res.status(200).json(retrievedTeachers);
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+});
+
+app.patch('/update/:id', async (req, res) => {
+    try {
+        const updatedTeacher = await teacherCrudOperations.update(Number.parseInt(req.params.id, 10), req.body);
+        return res.status(200).json(updatedTeacher);
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+});
+
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        const updatedTeacher = await teacherCrudOperations.delete(Number.parseInt(req.params.id, 10));
+        return res.status(200).json(updatedTeacher);
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+});
+
+app.listen(process.env.SERVER_PORT || 5000);
